fecb1821ad8a694f6bba2892455dc70fe420de81
Ruben Duran <rd@rubenduran.net>
2017-05-11 14:00:21 -0700

    Added form and retreive data with Flask app

---
+++ Python/python_stack/flask_fundamentals/name/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/process', methods=['POST'])
+def p():
+  name = request.form['name']
+  return render_template('process.html',name=name)
+
+app.run(debug=True)
