26e114199f178209b1b4811760a4e3470b51afd8
Ruben Duran <rd@rubenduran.net>
2017-05-11 17:55:13 -0700

    JS practice with jQuery

--- Webfundamentals/Javascript/jQ/weather/script.js
+++ Webfundamentals/Javascript/jQ/weather/script.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
         var aID = "6c32514ccb02a3376302977bf9ee55d7"
         var f = '&units=imperial'
         var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + f + "&&appid=" + aID;
+        console.log(url);
         $.get(url, function(res) {
             var temp = res.main.temp;
             var tempString = "<h1>" + city + "</h1><h4>The tempature is: " + temp + " f</h4>";
