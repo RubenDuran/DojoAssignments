3c85466de70179730df4858df875916cd757a206
Ruben Duran <rd@rubenduran.net>
2017-05-16 17:05:49 -0700

    mysql db practice

--- Python/assignments/testing.py
+++ Python/assignments/testing.py
@@ -1,13 +1,35 @@
-from flask import Flask  # Import Flask to allow us to create our app.
-# Global variable __name__ tells Flask whether or not we are running the file
+from flask import Flask, render_template
 app = Flask(__name__)
-# directly, or importing it as a module.
-
-
-# The "@" symbol designates a "decorator" which attaches the following
 @app.route('/')
-# function to the '/' route. This means that whenever we send a request to
-# localhost:5000/ we will run the following "hello_world" function.
-def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response.
-app.run(debug=True)      # Run the app in debug mode.
+def index():
+  return render_template("index.html", phrase="hello", times=5)
+app.run(debug=True)
+
+<html>
+    <head>
+      <title>My First Template</title>
+    </head>
+    <body>
+      <h3>My flask template with embedded Python-like code</h3>
+      <!-- this will output the value of our phrase variable -->
+      <p>Phrase: {{ phrase }}</p>
+      <!-- this will output the value of our times variable -->
+      <p>Times: {{ times }}</p>
+      <!-- here is an example of embedding a for-loop in our code -->
+      {% for x in range(0,times): %}
+      <p>{{ phrase }}</p>
+      {% endfor %}
+      <!-- here is an example of embedding an if statement in our code -->
+      {% if phrase == "hello" %}
+ <p>The phrase says hello</p>
+      {% endif %}
+    </body>
+</html>
+
+
+<!-- linking a css style sheet -->
+<link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='my_style_sheet.css') }}">
+<!-- linking a javascript file -->
+<script type="text/javascript" src="{{ url_for('static', filename='my_script.js') }}"></script>
+<!-- linking an image -->
+<img src="{{ url_for('static', filename='my_img.png') }}">
